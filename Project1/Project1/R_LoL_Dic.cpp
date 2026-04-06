d2833fbd24f7c4a0aeb495eec34c7f6f865fe90e
Yejun <dksdidtkachd@naver.com>
2026-04-07 05:33:21 +0900

    과제 완ㄹ료

--- Project1/Project1/R_LoL_Dic.cpp
+++ Project1/Project1/R_LoL_Dic.cpp
@@ -9,32 +9,36 @@ struct CharatherInfo {
 	int speed;
 	int range;
 	char position[100];
+
+	bool isvalue;
 };
 
 CharatherInfo LoL_Dic[100] = {
-	{"Jayce", 3466, 8307, 452, 73, "mid"},
-	{"Vayne", 3318, 2790, 390, 60, "support"},
-	{"Lee Sin", 9864, 6257, 747, 19, "support"},
-	{"Teemo", 1915, 1313, 700, 68, "mid"},
-	{"Nami", 2968, 2153, 803, 74, "support"},
-	{"Jinx", 2583, 3042, 123, 53, "jungle"},
-	{"Cassiopeia", 6067, 7330, 683, 24, "top"},
-	{"Ahri", 4573, 7079, 177, 79, "top"},
-	{"Fiora", 2591, 9329, 337, 81, "jungle"},
-	{"Zac", 9864, 7272, 405, 49, "bottom"},
-	{"Janna", 4922, 2275, 690, 66, "jungle"},
-	{"Ashe", 7421, 5549, 428, 31, "top"},
-	{"Twitch", 2980, 7703, 204, 91, "mid"},
-	{"Ezreal", 3660, 9835, 274, 44, "mid"},
-	{"Sona", 5655, 8442, 252, 26, "bottom"},
-	{"Lulu", 3231, 2014, 503, 32, "support"},
-	{"Amumu", 6763, 3994, 427, 19, "jungle"},
-	{"Master Yi", 5737, 7168, 434, 61, "top"},
-	{"Jarvan", 1895, 3070, 323, 80, "mid"},
-	{"Shen", 4794, 9220, 783, 42, "mid"},
-	{"Fizz", 9864, 1913, 433, 38, "bottom"},
-	{"Taric", 6158, 7451, 160, 36, "bottom" }
+	{"Jayce", 3466, 8307, 452, 73, "mid", true},
+	{"Vayne", 3318, 2790, 390, 60, "support", true},
+	{"Lee Sin", 9864, 6257, 747, 19, "support", true},
+	{"Teemo", 1915, 1313, 700, 68, "mid", true},
+	{"Nami", 2968, 2153, 803, 74, "support", true},
+	{"Jinx", 2583, 3042, 123, 53, "jungle", true},
+	{"Cassiopeia", 6067, 7330, 683, 24, "top", true},
+	{"Ahri", 4573, 7079, 177, 79, "top", true},
+	{"Fiora", 2591, 9329, 337, 81, "jungle", true},
+	{"Zac", 9864, 7272, 405, 49, "bottom", true},
+	{"Janna", 4922, 2275, 690, 66, "jungle", true},
+	{"Ashe", 7421, 5549, 428, 31, "top", true},
+	{"Twitch", 2980, 7703, 204, 91, "mid", true},
+	{"Ezreal", 3660, 9835, 274, 44, "mid", true},
+	{"Sona", 5655, 8442, 252, 26, "bottom",true},
+	{"Lulu", 3231, 2014, 503, 32, "support",true},
+	{"Amumu", 6763, 3994, 427, 19, "jungle",true},
+	{"Master Yi", 5737, 7168, 434, 61, "top",true},
+	{"Jarvan", 1895, 3070, 323, 80, "mid",true},
+	{"Shen", 4794, 9220, 783, 42, "mid",true},
+	{"Fizz", 9864, 1913, 433, 38, "bottom",true},
+	{"Taric", 6158, 7451, 160, 36, "bottom",true},
 };
+
+CharatherInfo list;
 
 int TextTest(const char* str1, const char* str2) {
 	while (*str1 != '\0' && (*str1 == *str2)) {
@@ -58,7 +62,7 @@ void Search(char str[], int temp) {
 		cout << "없습니다.";
 		return;
 	}
-	if (TextTest(LoL_Dic[temp].name, str) == 0) {
+	if (TextTest(LoL_Dic[temp].name, str) == 0 && LoL_Dic[temp].isvalue == true) {
 		cout << "Name : " << LoL_Dic[temp].name << endl;
 		cout << "HP : " << LoL_Dic[temp].hp << endl;
 		cout << "MP : " << LoL_Dic[temp].mp << endl;
@@ -78,7 +82,75 @@ void ChampSearch() {
 	Search(seararr, indexarr);
 }
 
+int InsertValue() {
+	int stat = 0;
+	while (true)
+	{
+		cin >> stat;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(1000, '\n');
+			cout << "정수만 입력" << endl;
+		}
+		else
+		{
+			if (cin.get() != '\n') {
+				cin.clear();
+				cin.ignore(1000, '\n');
+				cout << "정수만 입력" << endl;
+				continue;
+			}
+			return stat;
+		}
+	}
+}
+
+void Insert(int hp, int mp, int sp, int ra, int temp, char name[], char pos[]) {
+	if (LoL_Dic[temp].isvalue == false) {
+		cout << "들어옴 확인";
+		Strcpy(LoL_Dic[temp].name, name);
+		LoL_Dic[temp].hp = hp;
+		LoL_Dic[temp].mp = mp;
+		LoL_Dic[temp].speed = sp;
+		LoL_Dic[temp].range = ra;
+		Strcpy(LoL_Dic[temp].position, pos);
+		LoL_Dic[temp].isvalue = true;
+		return;
+	}
+	Insert(hp, mp, sp, ra, temp + 1, name, pos);
+}
+
 void ChampInsert() {
+	for (int i = 0; i < 100; i++) {
+		if (LoL_Dic[i].isvalue == false) {
+			break;
+		}
+		else if(i == 99) {
+			cout << "리스트가 가득 찼습니다.";
+		}
+	}
+	char namearr[100], posarr[100];
+	int h, m, s, r, indexarr = 0;
+	cout << "추가할 캐릭터의 이름을 입력 : ";
+	cin.getline(namearr, 100);
+	for (int i = 0; i < 100; i++) {
+		if (TextTest(namearr, LoL_Dic[i].name) == 0) {
+			cout << "동일한 이름의 챔피언 존재" << endl;
+			continue;
+		}
+	}
+	cout << "Hp : ";
+	h = InsertValue();
+	cout << "MP : ";
+	m = InsertValue();
+	cout << "Speed : ";
+	s = InsertValue();
+	cout << "Range : ";
+	r = InsertValue();
+	cout << "Position : ";
+	cin.getline(posarr, 100);
+	Insert(h, m, s, r, indexarr, namearr, posarr);
+	cout << "추가 완료" << endl;
 
 }
 
@@ -87,7 +159,7 @@ void Delete(char str[], int temp) {
 		cout << "없습니다." << endl;
 		return;
 	}
-	if (TextTest(LoL_Dic[temp].name, str) == 0) {
+	if (TextTest(LoL_Dic[temp].name, str) == 0 && LoL_Dic[temp].isvalue == true) {
 		LoL_Dic[temp] = {};
 		cout << "삭제 되었습니다.";
 		return;
@@ -104,30 +176,154 @@ void ChampDelete() {
 	Delete(delarr, indexarr);
 }
 
-void DeleteALL(char str[], int temp) {
+void DeleteALL(char str[], int temp, int &count) {
+	if (temp >= 100) {
+		return;
+	}
 	if (TextTest(LoL_Dic[temp].position, str) == 0) {
 		LoL_Dic[temp] = {};
+		count++;
 	}
+	DeleteALL(str, temp + 1, count);
+	
 }
 
 void ChampDeleteAll() {
 	char dearr[100];
-	int indexarr = 0;
+	int indexarr = 0, delcount = 0;
 	cout << "삭제할 포지션 선택 : ";
 	cin.getline(dearr, 100);
-	DeleteALL(dearr, indexarr);
+	DeleteALL(dearr, indexarr, delcount);
+	if (delcount == 0) {
+		cout << "리스트에 찾는 포지션이 존재하지 않습니다." << endl;
+	}
+	else {
+		cout << "삭제 완료" << endl;
+	}
 }
 
-void ChampPrintAll() {
-	
+void ChampPrintAll(int temp) {
+	if (temp >= 100) {
+		return;
+	}
+	if (LoL_Dic[temp].isvalue == true) {
+		cout << LoL_Dic[temp].name << ", ";
+		cout << LoL_Dic[temp].hp << ", ";
+		cout << LoL_Dic[temp].mp << ", ";
+		cout << LoL_Dic[temp].speed << ", ";
+		cout << LoL_Dic[temp].range << ", ";
+		cout << LoL_Dic[temp].position << endl;
+	}
+	ChampPrintAll(temp + 1);
+}
+
+void FindMaxHp(int temp, int top) {
+	if (temp >= 100) {
+		return;
+	}
+	if (top == LoL_Dic[temp].hp) {
+		cout << "Name : " << LoL_Dic[temp].name << endl;
+		cout << "HP : " << LoL_Dic[temp].hp << endl;
+		cout << "MP : " << LoL_Dic[temp].mp << endl;
+		cout << "Speed : " << LoL_Dic[temp].speed << endl;
+		cout << "Range : " << LoL_Dic[temp].range << endl;
+		cout << "Position : " << LoL_Dic[temp].position << endl << endl;
+	}
+	FindMaxHp(temp + 1, top);
 }
 
 void ChampFMH() {
+	int king = 0, indexarr = 0;
+	for (int i = 0; i < 100; i++) {
+		if (LoL_Dic[i].isvalue == true) {
+			king = LoL_Dic[i].hp;
+			break;
+		}
+	}
+	for (int i = 0; i < 100; i++) {
+		if (king < LoL_Dic[i].hp && LoL_Dic[i].isvalue == true) {
+			king = LoL_Dic[i].hp;
+		}
+	}
+	FindMaxHp(indexarr, king);
+}
 
+void ExratctTemp(CharatherInfo temp[], int& count, int index) {
+	if (index >= 100) {
+		return;
+	}
+	if (LoL_Dic[index].isvalue == true) {
+		temp[count] = LoL_Dic[index];
+		count++;
+	}
+	ExratctTemp(temp, count, index + 1);
+}
+
+void BubbleInner(CharatherInfo temp[], int max, int index) {
+	if (index >= max) {
+		return;
+	}
+	if (temp[index].hp < temp[index + 1].hp) {
+		list = temp[index];
+		temp[index] = temp[index + 1];
+		temp[index + 1] = list;
+	}
+	BubbleInner(temp, max, index + 1);
+}
+
+void BubbleOuter(CharatherInfo temp[], int n) {
+	if (n <= 1) {
+		return;
+	}
+	BubbleInner(temp, n - 1, 0);
+	BubbleOuter(temp, n - 1);
+}
+
+void InjectTemp(CharatherInfo temp[], int& count, int index) {
+	if (index >= 100) {
+		return;
+	}
+	if (LoL_Dic[index].isvalue == true) {
+		LoL_Dic[index] = temp[count];
+		count++;
+	}
+	InjectTemp(temp, count, index + 1);
 }
 
 void ChampSBH() {
+	CharatherInfo temp[100] = {};
+	int count = 0;
 
+	ExratctTemp(temp, count, 0);
+
+	BubbleOuter(temp, count);
+	count = 0;
+
+	InjectTemp(temp, count, 0);
+
+	cout << "정렬 완료" << endl;
+	//for (int i = 0; i < 100; i++) {
+	//	if (LoL_Dic[i].isvalue == true) {
+	//		temp[count] = LoL_Dic[i];
+	//		count++;
+	//	}
+	//}
+	//for (int i = 0; i < count-1; i++) {
+	//	for (int j = 0; j < count - 1; j++) {
+	//		if (temp[j].hp < temp[j + 1].hp) {
+	//			list = temp[j];
+	//			temp[j] = temp[j + 1];
+	//			temp[j + 1] = list;
+	//		}
+	//	}
+	//}
+	//count = 0;
+	//for (int i = 0; i < 100; i++) {
+	//	if (LoL_Dic[i].isvalue == true) {
+	//		LoL_Dic[i] = temp[count];
+	//		count++;
+	//	}
+	//}
 }
 
 void HelpRule() {
@@ -150,6 +346,7 @@ int main() {
 			ChampSearch();
 		}
 		else if (TextTest(input, "Insert") == 0) {
+			CharatherInfo Insert_Info;
 			ChampInsert();
 		}
 		else if (TextTest(input, "Delete") == 0) {
@@ -159,7 +356,8 @@ int main() {
 			ChampDeleteAll();
 		}
 		else if (TextTest(input, "PrintAll") == 0) {
-			ChampPrintAll();
+			int printcount = 0;
+			ChampPrintAll(printcount);
 		}
 		else if (TextTest(input, "FindMaxHp") == 0) {
 			ChampFMH();
