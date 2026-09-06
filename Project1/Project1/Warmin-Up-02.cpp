ec71bcb609f10083e80ef1911a2e275f697f7083
Yejun <dksdidtkachd@naver.com>
2026-09-07 01:33:09 +0900

    1212

---
+++ Project1/Project1/Warmin-Up-02.cpp
@@ -0,0 +1,201 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <Windows.h>
+
+using namespace std;
+
+vector<string> fullData;
+
+void Print() {
+	for (int i = 0; i < fullData.size(); i++) {
+		stringstream ss(fullData[i]);
+		string word;
+		bool frist = true;
+		while (ss >> word) {
+			if (!frist) cout << " ";
+			cout << word;
+			frist = false;
+		}
+		cout << endl;
+	}
+}
+
+void FileOpen() {
+	string filename;
+	cout << "불러올 파일 이름을 입력하세요 (ex : data.txt) : ";
+	cin >> filename;
+	fstream file(filename);
+	if (!file.is_open()) {
+		cout << "존재하지 않는 파일이거나 파일을 불러올 수 없습니다." << endl;
+		return;
+	}
+
+	string line;
+	while (getline(file, line)) {
+		fullData.push_back(line);
+	}
+	cout << fullData.size() << "개의 문장 저장" << endl;
+	Print();
+}
+
+
+void TurnLoRo(){
+	string text;
+	for (int i = 0; i < fullData.size(); i++) {
+		for (int j = 0; j < fullData[i].size(); j++) {
+			if (fullData[i][j] >= 65 && fullData[i][j] <= 90) {
+				fullData[i][j] += 32;
+			}
+			else if (fullData[i][j] >= 97 && fullData[i][j] <= 122) {
+				fullData[i][j] -= 32;
+			}
+		}
+	}
+
+}
+
+void PrintWord() {
+	int wordCount = 0;
+	for (int i = 0; i < fullData.size(); i++) {
+		stringstream ss(fullData[i]);
+		string word;
+		bool frist = true;
+		while (ss >> word) {
+			if (!frist) cout << " ";
+			cout << word;
+			wordCount++;
+			frist = false;
+		}
+		cout << setw(5) << "단어 개수 : " << wordCount << endl;
+		wordCount = 0;
+	}
+}
+
+void ChangeColor(bool compare) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+	GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+	WORD originalAttributes = consoleInfo.wAttributes;
+
+	int colorCount = 0;
+	if (compare == false) {
+		for (int i = 0; i < fullData.size(); i++) {
+			stringstream ss(fullData[i]);
+			string word;
+			bool frist = true;
+			while (ss >> word) {
+				if (word[0] >= 'A' && word[0] <= 'Z') {
+					SetConsoleTextAttribute(hConsole, 6);
+					colorCount++;
+				}
+				if (!frist) cout << " ";
+				cout << word;
+				SetConsoleTextAttribute(hConsole, originalAttributes);
+				frist = false;
+			}
+			cout << setw(5) << "단어 개수 : " << colorCount << endl;
+			colorCount = 0;
+		}
+	}
+	else {
+		SetConsoleTextAttribute(hConsole, originalAttributes);
+		Print();
+	}
+}
+
+void BackWord(bool compare) {
+	if (compare == true) {
+		Print();
+	}
+	else {
+		for (int i = 0; i < fullData.size(); i++) {
+			int dataSize = fullData[i].size();
+			for (int j = dataSize - 1; j >= 0; j--) {
+				cout << fullData[i][j];
+			}
+			cout << endl;
+		}
+	}
+}
+
+void StarPrint(bool compare) {
+	if (compare == true) {
+		Print();
+	}
+	else {
+		for (int i = 0; i < fullData.size(); i++) {
+			stringstream ss(fullData[i]);
+			string word;
+			bool frist = true;
+			while (ss >> word) {
+				if (!frist) cout << "*";
+				cout << word;
+				frist = false;
+			}
+			cout << endl;
+		}
+	}
+}
+
+void ReverseWordOrder() {
+	for (int i = 0; i < fullData.size(); i++) {
+		stringstream ss(fullData[i]);
+		vector<string> words;
+		string word;
+		while (ss >> word) {
+			words.push_back(word); 
+		}
+		for (int j = words.size() - 1; j >= 0; j--) { 
+			if (j != words.size() - 1) cout << " ";
+			cout << words[j];
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	FileOpen();
+	string input;
+	bool compare = false;
+	bool compare2 = false;
+	bool compare3 = false;
+	while (true) {
+		cin >> input;
+		if (input == "a") {
+			system("cls");
+			TurnLoRo();
+			Print();
+		}
+		else if (input == "b") {
+			PrintWord();
+		}
+		else if (input == "c") {
+			system("cls");
+			ChangeColor(compare);
+			compare = !compare;
+		}
+		else if (input == "d") {
+			system("cls");
+			BackWord(compare3);
+			compare3 = !compare3;
+		}
+		else if (input == "e") {
+			system("cls");
+			StarPrint(compare2);
+			compare2 = !compare2;
+		}
+		else if (input == "f") {
+			ReverseWordOrder();
+		}
+		else if (input == "q") {
+			break;
+		}
+
+	}
+	return 0;
+}
--- Project1/Project1/Warming-Up_01.cpp
+++ Project1/Project1/Warming-Up_01.cpp
@@ -11,6 +11,7 @@ struct Determinant
 
 Determinant det1;
 Determinant det2;
+Determinant originalDet1, originalDet2;
 
 void MadeBoard() {
 	random_device rd1;
@@ -41,6 +42,23 @@ void MadeBoard() {
 	cout << endl;
 }
 
+void Print() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			cout << setw(5) << det1.determinant[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < 4; i++) {
+		cout << endl;
+		for (int j = 0; j < 4; j++) {
+			cout << setw(5) << det2.determinant[i][j];
+		}
+	}
+	cout << endl;
+}
+
 void DeMulti() {
 	Determinant muldet;
 	for (int t = 0; t < 4; t++) {
@@ -49,7 +67,7 @@ void DeMulti() {
 			for (int j = 0; j < 4; j++) {
 				multipul += det1.determinant[i][j] * det2.determinant[j][t];
 			}
-			muldet.determinant[t][i] = multipul;
+			muldet.determinant[i][t] = multipul;
 		}
 	}
 	for (int i = 0; i < 4; i++) {
@@ -60,11 +78,249 @@ void DeMulti() {
 	}
 }
 
+void DeAdd() {
+	Determinant addet;
+	for (int i = 0; i < 4; i++) {
+		int adder = 0;
+		for (int j = 0; j < 4; j++) {
+			addet.determinant[i][j] = det1.determinant[i][j] + det2.determinant[i][j];
+		}
+	}
+	for (int i = 0; i < 4; i++) {
+		cout << endl;
+		for (int j = 0; j < 4; j++) {
+			cout << setw(5) << addet.determinant[i][j];
+		}
+	}
+}
+
+void DeMinus() {
+	Determinant mindet;
+	for (int i = 0; i < 4; i++) {
+		int adder = 0;
+		for (int j = 0; j < 4; j++) {
+			mindet.determinant[i][j] = det1.determinant[i][j] - det2.determinant[i][j];
+		}
+	}
+	for (int i = 0; i < 4; i++) {
+		cout << endl;
+		for (int j = 0; j < 4; j++) {
+			cout << setw(5) << mindet.determinant[i][j];
+		}
+	}
+}
+
+int getDeterminant(int mat[4][4], int n) {
+	if (n == 2) {
+		return mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
+	}
+	int result = 0;
+	for (int col = 0; col < n; col++) {
+		int sub[4][4];
+		int subRow = 0;
+		for (int i = 1; i < n; i++) {
+			int subCol = 0;
+			for (int j = 0; j < n; j++) {
+				if (j == col) continue;
+				sub[subRow][subCol] = mat[i][j];
+				subCol++;
+			}
+			subRow++;
+		}
+		int sign = (col % 2 == 0) ? 1 : -1;
+		result += sign * mat[0][col] * getDeterminant(sub, n - 1);
+	}
+	return result;
+}
+
+void TransPosed() {
+	Determinant temp1 = det1;
+	Determinant temp2 = det2;
+	Determinant result1, result2;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			result1.determinant[i][j] = temp1.determinant[j][i];
+			result2.determinant[i][j] = temp2.determinant[j][i];
+		}
+	}
+	det1 = result1;
+	det2 = result2;
+
+	int a = getDeterminant(det1.determinant, 4);
+	int b = getDeterminant(det2.determinant, 4);
+	Print();
+	cout << endl;
+	cout << a << setw(5) << b << endl;
+}
+
+void RowMin(bool rowCompare) {
+	if (rowCompare) {
+		originalDet1 = det1;
+		originalDet2 = det2;
+		int a = 10;
+		int b = 10;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (det1.determinant[i][j] < a) {
+					a = det1.determinant[i][j];
+				}
+			}
+			for (int t = 0; t < 4; t++) {
+				det1.determinant[i][t] -= a;
+			}
+			a = 10;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (det2.determinant[i][j] < b) {
+					b = det2.determinant[i][j];
+				}
+			}
+			for (int t = 0; t < 4; t++) {
+				det2.determinant[i][t] -= b;
+			}
+			b = 10;
+		}
+	}
+	else {
+		det1 = originalDet1;
+		det2 = originalDet2;
+	}
+	system("cls");
+	Print();
+}
+
+void ColMax(bool colCompare) {
+	if (colCompare) {
+		originalDet1 = det1;
+		originalDet2 = det2;
+		int a = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (det1.determinant[j][i] > a) {
+					a = det1.determinant[j][i];
+				}
+			}
+			for (int t = 0; t < 4; t++) {
+				det1.determinant[t][i] += a;
+			}
+			a = 0;
+		}
+		a = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (det2.determinant[j][i] > a) {
+					a = det2.determinant[j][i];
+				}
+			}
+			for (int t = 0; t < 4; t++) {
+				det2.determinant[t][i] += a;
+			}
+			a = 0;
+		}
+	}
+	else {
+		det1 = originalDet1;
+		det2 = originalDet2;
+	}
+	system("cls");
+	Print();
+}
+
+void Plus() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			det1.determinant[i][j] += 1;
+			if (det1.determinant[i][j] == 10) {
+				det1.determinant[i][j] = 0;
+			}
+		}
+	}
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			det2.determinant[i][j] += 1;
+			if (det2.determinant[i][j] == 10) {
+				det2.determinant[i][j] = 0;
+			}
+		}
+	}
+	Print();
+}
+
+void Minus() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			det1.determinant[i][j] -= 1;
+			if (det1.determinant[i][j] == -1) {
+				det1.determinant[i][j] = 9;
+			}
+		}
+	}
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			det2.determinant[i][j] -= 1;
+			if (det2.determinant[i][j] == -1) {
+				det2.determinant[i][j] = 9;
+			}
+		}
+	}
+	Print();
+}
+
+
 int main() {
 	MadeBoard();
-	int input = 0;
-	cin >> input;
-	if (input == 1) {
-		DeMulti();
+	string input;
+	bool compare1 = false;
+	bool compare2 = false;
+	while (true) {
+
+		cin >> input;
+		if (input == "m") {
+			DeMulti();
+		}
+		else if (input == "a") {
+			DeAdd();
+		}
+		else if (input == "d") {
+			DeMinus();
+		}
+		else if (input == "r") {
+			int result1 = getDeterminant(det1.determinant, 4);
+			int result2 = getDeterminant(det2.determinant, 4);
+			cout << result1 << setw(5) << result2;
+		}
+		else if (input == "t") {
+			TransPosed();
+		}
+		else if (input == "e") {
+			compare1 = !compare1;
+			RowMin(compare1);
+		}
+		else if (input == "f") {
+			compare2 = !compare2;
+			ColMax(compare2);
+		}
+		else if (input == "+") {
+			Plus();
+		}
+		else if (input == "-") {
+			Minus();
+		}
+		else if (input == "s") {
+			system("cls");
+			MadeBoard();
+		}
+		else if (input == "q") {
+			break;
+		}
+		else {
+			cout << "잘못 입력" << endl;
+			continue;
+
+		}
 	}
+	return 0;
 }
